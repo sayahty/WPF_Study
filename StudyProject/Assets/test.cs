698ad4ec13ad9d9ea1d5c22a4e733f59d872060d
sayahty <sayahty@yahoo.ca>
2020-06-28 20:03:44 +0800

    1

---
+++ StudyProject/Assets/test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace celia.game
+{
+	public class test : MonoBehaviour
+	{
+		// Start is called before the first frame update
+		void Start()
+		{
+			
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+            GetComponent<Text>().text = "123";
+		}
+	}
+}
