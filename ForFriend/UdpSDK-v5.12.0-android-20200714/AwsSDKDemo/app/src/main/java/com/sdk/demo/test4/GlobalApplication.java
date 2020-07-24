8abe0ab15a77350a4b00212c9cc4130dd75bf24a
sayahty <sayahty@yahoo.ca>
2020-07-24 17:06:28 +0800

    upload forfriend

---
+++ ForFriend/UdpSDK-v5.12.0-android-20200714/AwsSDKDemo/app/src/main/java/com/sdk/demo/test4/GlobalApplication.java
@@ -0,0 +1,14 @@
+package com.sdk.demo.test4;
+
+import com.games.sdk.SdkApplication;
+
+public class GlobalApplication extends SdkApplication {
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		
+		// other code
+	}
+
+}
