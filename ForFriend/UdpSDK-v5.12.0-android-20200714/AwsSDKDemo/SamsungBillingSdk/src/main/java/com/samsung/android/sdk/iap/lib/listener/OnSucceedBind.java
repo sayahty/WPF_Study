722da4a1d436f7109b7311df0ee3c0d846b3c803
sayahty <sayahty@yahoo.ca>
2020-09-28 11:04:50 +0800

    2

--- ForFriend/UdpSDK-v5.12.0-android-20200714/AwsSDKDemo/SamsungBillingSdk/src/main/java/com/samsung/android/sdk/iap/lib/listener/OnSucceedBind.java
+++
@@ -1,8 +0,0 @@
-package com.samsung.android.sdk.iap.lib.listener;
-
-/**
- * Created by sangbum7.kim on 2018-02-28.
- */
-
-public interface OnSucceedBind {
-}
---
+++ updateTest/Assets/test.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+namespace celia.game
+{
+	public class test : MonoBehaviour
+	{
+        public Text t;
+		public void Test()
+        {
+            UnityWebRequest r = UnityWebRequest.Get("https://xlycs.res.rastargame.com/NetAssets_Test/GameSetting.json");
+
+            r.SendWebRequest();
+
+            while (!r.isDone)
+            {
+                System.Threading.Thread.Sleep(1);
+            }
+
+            if (r.error == null)
+            {
+                string content = DownloadHandlerBuffer.GetContent(r);
+                t.text = content;
+            }
+            else
+            {
+                Debug.Log("Download error:" + r.error);
+            }
+        }
+	}
+}
